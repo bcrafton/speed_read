ad8c98c15b4b762628fc403a8ae2450e811e4569
Brian Crafton <crafton.b@husky.neu.edu>
2020-03-04 18:44:38 -0500

    ssdfa.master model

--- Model.py
+++ Model.py
@@ -17,94 +17,123 @@ class Model:
 
     def get_weights(self):
         weights = {}
-
         for ii in range(self.num_layers):
-            tups = self.layers[ii].get_weights()
-            for (k, v) in tups:
-                weights[k] = v
-
+            l = self.layers[ii]
+            tup = l.get_weights()
+            for (key, value) in tup:
+                weights[key] = value
+            
         return weights
 
     def predict(self, X):
         A = [None] * self.num_layers
-        cache = [None] * self.num_layers
         
         for ii in range(self.num_layers):
             l = self.layers[ii]
             if ii == 0:
-                A[ii], cache[ii] = l.forward(X)
+                A[ii] = l.forward(X)
             else:
-                A[ii], cache[ii] = l.forward(A[ii-1])
+                A[ii] = l.forward(A[ii-1]['aout'])
                 
-        return A[self.num_layers-1]
-        
-    def predict1(self, X):
-        A = [None] * self.num_layers
-        cache = [None] * self.num_layers
-        
-        for ii in range(self.num_layers):
-            l = self.layers[ii]
-            if ii == 0:
-                A[ii], cache[ii] = l.forward1(X)
-            else:
-                A[ii], cache[ii] = l.forward1(A[ii-1])
-                
-        return A, cache
-    
-    def predict2(self, X):
-        A = [None] * self.num_layers
-        cache = [None] * self.num_layers
-        
-        for ii in range(self.num_layers):
-            l = self.layers[ii]
-            if ii == 0:
-                A[ii], cache[ii] = l.forward2(X)
-            else:
-                A[ii], cache[ii] = l.forward2(A[ii-1])
-                
-        return A[self.num_layers-1], A
+        return A[self.num_layers-1]['aout']
     
     ####################################################################
       
     def gvs(self, X, Y):
         A = [None] * self.num_layers
-        cache = [None] * self.num_layers
-
         D = [None] * self.num_layers
         grads_and_vars = []
         
         for ii in range(self.num_layers):
             l = self.layers[ii]
             if ii == 0:
-                A[ii], cache[ii] = l.forward(X)
+                A[ii] = l.forward(X)
             else:
-                A[ii], cache[ii] = l.forward(A[ii-1])
+                A[ii] = l.forward(A[ii-1]['aout'])
 
-        E = tf.nn.softmax(A[self.num_layers-1]) - Y
-        # N = tf.shape(A[self.num_layers-1])[0]
-        # N = tf.cast(N, dtype=tf.float32)
-        # E = E / N
+        E = tf.nn.softmax(A[self.num_layers-1]['aout']) - Y
+        N = tf.shape(A[self.num_layers-1]['aout'])[0]
+        N = tf.cast(N, dtype=tf.float32)
+        E = E / N
             
         for ii in range(self.num_layers-1, -1, -1):
             l = self.layers[ii]
             
             if (ii == self.num_layers-1):
-                D[ii], gvs = l.bp(A[ii-1], A[ii], E,       cache[ii])
-                grads_and_vars = gvs + grads_and_vars
+                D[ii], gvs = l.bp(A[ii-1]['aout'], A[ii]['aout'], E,               A[ii]['cache'])
+                grads_and_vars.extend(gvs)
             elif (ii == 0):
-                D[ii], gvs = l.bp(X,       A[ii], D[ii+1], cache[ii])
-                grads_and_vars = gvs + grads_and_vars
+                D[ii], gvs = l.bp(X,               A[ii]['aout'], D[ii+1]['dout'], A[ii]['cache'])
+                grads_and_vars.extend(gvs)
             else:
-                D[ii], gvs = l.bp(A[ii-1], A[ii], D[ii+1], cache[ii])
-                grads_and_vars = gvs + grads_and_vars
+                D[ii], gvs = l.bp(A[ii-1]['aout'], A[ii]['aout'], D[ii+1]['dout'], A[ii]['cache'])
+                grads_and_vars.extend(gvs)
 
         return grads_and_vars
     
     def dfa_gvs(self, X, Y):
-        assert(False)
+        A = [None] * self.num_layers
+        D = [None] * self.num_layers
+        grads_and_vars = []
+        
+        for ii in range(self.num_layers):
+            l = self.layers[ii]
+            if ii == 0:
+                A[ii] = l.forward(X)
+            else:
+                A[ii] = l.forward(A[ii-1]['aout'])
+
+        E = tf.nn.softmax(A[self.num_layers-1]['aout']) - Y
+        N = tf.shape(A[self.num_layers-1]['aout'])[0]
+        N = tf.cast(N, dtype=tf.float32)
+        E = E / N
+            
+        for ii in range(self.num_layers-1, -1, -1):
+            l = self.layers[ii]
+
+            if (ii == self.num_layers-1):
+                D[ii], gvs = l.dfa(A[ii-1]['aout'], A[ii]['aout'], E, E,               A[ii]['cache'])
+                grads_and_vars.extend(gvs)
+            elif (ii == 0):
+                D[ii], gvs = l.dfa(X,               A[ii]['aout'], E, D[ii+1]['dout'], A[ii]['cache'])
+                grads_and_vars.extend(gvs)
+            else:
+                D[ii], gvs = l.dfa(A[ii-1]['aout'], A[ii]['aout'], E, D[ii+1]['dout'], A[ii]['cache'])
+                grads_and_vars.extend(gvs)
+                
+        return grads_and_vars
 
     def lel_gvs(self, X, Y):
-        assert(False)
+        A = [None] * self.num_layers
+        D = [None] * self.num_layers
+        grads_and_vars = []
+        
+        for ii in range(self.num_layers):
+            l = self.layers[ii]
+            if ii == 0:
+                A[ii] = l.forward(X)
+            else:
+                A[ii] = l.forward(A[ii-1]['aout'])
+
+        E = tf.nn.softmax(A[self.num_layers-1]['aout']) - Y
+        N = tf.shape(A[self.num_layers-1]['aout'])[0]
+        N = tf.cast(N, dtype=tf.float32)
+        E = E / N
+            
+        for ii in range(self.num_layers-1, -1, -1):
+            l = self.layers[ii]
+
+            if (ii == self.num_layers-1):
+                D[ii], gvs = l.lel(A[ii-1]['aout'], A[ii]['aout'], E, E,               Y, A[ii]['cache'])
+                grads_and_vars.extend(gvs)
+            elif (ii == 0):
+                D[ii], gvs = l.lel(X,               A[ii]['aout'], E, D[ii+1]['dout'], Y, A[ii]['cache'])
+                grads_and_vars.extend(gvs)
+            else:
+                D[ii], gvs = l.lel(A[ii-1]['aout'], A[ii]['aout'], E, D[ii+1]['dout'], Y, A[ii]['cache'])
+                grads_and_vars.extend(gvs)
+                
+        return grads_and_vars
 
     ####################################################################
     
