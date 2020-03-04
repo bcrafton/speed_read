f48ea033f19e9ed33ea3623ca5269348a648a96e
Brian Crafton <crafton.b@husky.neu.edu>
2020-03-04 16:24:41 -0500

    set up tb

--- emu.py
+++ emu.py
@@ -1,14 +1,10 @@
 
 import numpy as np
-from conv_utils import conv_output_length
-from dot import *
-
 np.set_printoptions(threshold=np.inf)
 
-###########################################
-
-OPCODE_CONV = 1
-OPCODE_DOT = 2
+from conv_utils import conv_output_length
+from dot import *
+from defines import *
 
 ###########################################
 
@@ -60,37 +56,38 @@ def dot_ref(x, w, b, q):
 
 ###########################################
 
-params = np.load('emu.npy', allow_pickle=True).item()
+def emu(path):
 
-######################
+    params = np.load('./%s/emu.npy' % (path), allow_pickle=True).item()
 
-patch = [[None for col in range(params['num_layer'])] for row in range(params['num_example'])] 
-yout  = [[None for col in range(params['num_layer'])] for row in range(params['num_example'])] 
+    ######################
 
-for ii in range(params['num_example']):
-    for jj in range(params['num_layer']):
-        
-        param = params[jj]
-    
-        #######
-        
-        xin = params['x'][ii] if (jj == 0) else yout[ii][jj-1]
-        xin = np.reshape(xin, param['x'])
-        xin = xin.astype(int)
-        
-        #######
+    patch = [[None for col in range(params['num_layer'])] for row in range(params['num_example'])] 
+    yout  = [[None for col in range(params['num_layer'])] for row in range(params['num_example'])] 
 
-        if param['op'] == OPCODE_CONV: 
-            y        = conv(np.copy(xin), param['weights'], param['bias'], param['quant'], param['dims']['stride'], param['dims']['pad1'], param['dims']['pad2'])
-            y_ref, _ = conv_ref(np.copy(xin), param['weights'], param['bias'], param['quant'], param['dims']['stride'], param['dims']['pad1'], param['dims']['pad2'])
-            assert (np.all(y == y_ref))
-
-        else:
-            y        = dot(np.copy(xin), param['weights'], param['bias'], param['quant'])
-            y_ref, _ = dot_ref(np.copy(xin), param['weights'], param['bias'], param['quant'])
-            assert (np.all(y == y_ref))
+    for ii in range(params['num_example']):
+        for jj in range(params['num_layer']):
             
-        yout[ii][jj] = y
+            param = params[jj]
+        
+            #######
+            
+            xin = params['x'][ii] if (jj == 0) else yout[ii][jj-1]
+            xin = np.reshape(xin, param['x'])
+            xin = xin.astype(int)
+            
+            #######
+
+            if param['op'] == OPCODE_CONV: 
+                y        = conv(np.copy(xin), param['weights'], param['bias'], param['quant'], param['dims']['stride'], param['dims']['pad1'], param['dims']['pad2'])
+                y_ref, _ = conv_ref(np.copy(xin), param['weights'], param['bias'], param['quant'], param['dims']['stride'], param['dims']['pad1'], param['dims']['pad2'])
+                assert (np.all(y == y_ref))
+            else:
+                y        = dot(np.copy(xin), param['weights'], param['bias'], param['quant'])
+                y_ref, _ = dot_ref(np.copy(xin), param['weights'], param['bias'], param['quant'])
+                assert (np.all(y == y_ref))
+                
+            yout[ii][jj] = y
 
 ######################
 
