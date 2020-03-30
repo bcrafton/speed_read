72b152257ab698d540e13f8a0a881a8a1658d0ae
Brian Crafton <crafton.b@husky.neu.edu>
2020-03-30 10:26:53 -0400

    this is a problem

--- src/print_results.py
+++ src/print_results.py
@@ -19,29 +19,14 @@ def merge_dicts(list_of_dicts):
 
 comp_pJ = 22. * 1e-12 / 32. / 16.
 
-num_layers = 7
+num_layers = 6
 num_comparator = 8
 results = np.load('results.npy', allow_pickle=True).item()
-results_tf = np.load('results_tf.npy', allow_pickle=True).item()
 
 x = np.array([0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15])
 
 y_mean = np.zeros(shape=(2, 2, len(x), num_layers))
 y_std = np.zeros(shape=(2, 2, len(x), num_layers))
-
-y_mac_per_cycle = np.zeros(shape=(2, 2, len(x), num_layers))
-y_mac_per_pJ = np.zeros(shape=(2, 2, len(x), num_layers))
-
-y_mac = np.zeros(shape=(2, 2, len(x), num_layers))
-y_cycle = np.zeros(shape=(2, 2, len(x), num_layers))
-
-y_ron = np.zeros(shape=(2, 2, len(x), num_layers))
-y_roff = np.zeros(shape=(2, 2, len(x), num_layers))
-y_adc = np.zeros(shape=(2, 2, len(x), num_layers, num_comparator))
-
-y_energy = np.zeros(shape=(2, 2, len(x), num_layers))
-
-acc = results_tf['acc_tf']
 
 for key in sorted(results.keys()):
     (skip, cards, sigma) = key
@@ -54,21 +39,6 @@ for key in sorted(results.keys()):
         y_mean[skip][cards][sigma_index][layer] = np.mean(example_results['mean'])
         y_std[skip][cards][sigma_index][layer] = np.mean(example_results['std'])
 
-        y_mac_per_cycle[skip][cards][sigma_index][layer]  = np.sum(example_results['nmac']) / np.sum(example_results['cycle'])
-        y_mac[skip][cards][sigma_index][layer]   = np.mean(example_results['nmac']) 
-        y_cycle[skip][cards][sigma_index][layer] = np.mean(example_results['cycle'])
-
-        y_ron[skip][cards][sigma_index][layer] = np.sum(example_results['ron'])
-        y_roff[skip][cards][sigma_index][layer] = np.sum(example_results['roff'])
-        y_adc[skip][cards][sigma_index][layer] = np.sum(example_results['adc'], axis=0)
-        
-        y_energy[skip][cards][sigma_index][layer] += y_ron[skip][cards][sigma_index][layer] * 2e-16
-        y_energy[skip][cards][sigma_index][layer] += y_roff[skip][cards][sigma_index][layer] * 2e-16
-        y_energy[skip][cards][sigma_index][layer] += np.sum(y_adc[skip][cards][sigma_index][layer] * np.array([1,2,3,4,5,6,7,8]) * comp_pJ)
-        # print (skip, cards, y_adc[skip][cards][sigma_index][layer] * np.array([1,2,3,4,5,6,7,8]))
-
-        y_mac_per_pJ[skip][cards][sigma_index][layer] = np.sum(example_results['nmac']) / 1e12 / np.sum(y_energy[skip][cards][sigma_index][layer])
-
 ####################
 
 print ('mean')
@@ -80,16 +50,6 @@ print ('std')
 # print (np.around(y_std[0, 0],  3))
 print (np.around(y_std[1, 0],  3))
 # print (np.around(y_std[1, 1],  3))
-
-print ('mac / cycle')
-# print (np.around(y_mac_per_cycle[0, 0], 1))
-print (np.around(y_mac_per_cycle[1, 0], 1))
-# print (np.around(y_mac_per_cycle[1, 1], 1))
-
-print ('mac / pJ')
-# print (np.around(y_mac_per_pJ[0, 0],  3))
-print (np.around(y_mac_per_pJ[1, 0],  3))
-# print (np.around(y_mac_per_pJ[1, 1],  3))
 
 ####################
 
