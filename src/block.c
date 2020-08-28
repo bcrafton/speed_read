88e577535449f3376a3d351426d99203179cae94
Brian Crafton <crafton.b@husky.neu.edu>
2020-08-28 19:29:17 -0400

    elegant

--- src/block.c
+++ src/block.c
@@ -17,8 +17,10 @@ Block::Block(int block_id, int* x, int* w, int* y, Params* params) {
 int Block::pim(int row) {
   int rpr_addr = this->xb * 8 + this->col;
   int rpr = this->params->lut_rpr[rpr_addr];
-  assert(rpr_addr >= 0); assert(rpr_addr < 64); 
-  assert(rpr > 0); assert(rpr <= 64);
+  assert(rpr_addr >= 0);
+  assert(rpr_addr < 64); 
+  assert(rpr > 0);
+  assert(rpr <= 64);
 
   int done = 0;
   for (int i=0; i<this->params->NBL; i++) {
@@ -26,4 +28,15 @@ int Block::pim(int row) {
     if (i == 0) done = ret;
     else        assert(ret == done);
   } 
+  
+  if (done) {
+    this->col = (this->col + 1) % 8;
+    if (this->col == 0) {
+      this->xb = (this->xb + 1) % 8;
+      if (this->xb == 0) {
+        return 1;
+      }
+    }
+  }
+  return 0;
 }
