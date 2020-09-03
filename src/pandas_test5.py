7f48e00d2e50df580e28270874a580de458aadbc
Brian Crafton <crafton.b@husky.neu.edu>
2020-09-03 15:02:35 -0400

    nice

--- src/pandas_test5.py
+++ src/pandas_test5.py
@@ -31,25 +31,41 @@ df = pd.DataFrame.from_dict(results)
 
 ####################
 
+layer = 3
 comp_pJ = 22. * 1e-12 / 32. / 16.
 
 for cards in [0, 1]:
     for rpr_alloc in ['dynamic', 'centroids']:
-        query = '(rpr_alloc == "%s") & (cards == %d) & (id == 5)' % (rpr_alloc, cards)
+    
+        ######################################
+    
+        query = '(rpr_alloc == "%s") & (cards == %d) & (id == %d)' % (rpr_alloc, cards, layer)
         samples = df.query(query)
         if (len(samples) == 0):
             continue
 
+        ######################################
+
         adc = np.stack(samples['adc'], axis=0)    
-        
         energy = np.sum(np.array([1,2,3,4,5,6,7,8]) * adc * comp_pJ, axis=1) 
         energy += samples['ron'] * 2e-16
         energy += samples['roff'] * 2e-16
-        
         mac_per_pJ = samples['nmac'] / 1e12 / energy
+        
+        ######################################
+        
+        error = samples['std']
+        
+        ######################################
+        
+        mac_per_cycle = samples['nmac'] / samples['cycle']
+        
+        ######################################
 
         sigma = samples['sigma']
-        plt.scatter(sigma, mac_per_pJ, label=rpr_alloc)
+        plt.plot(sigma, mac_per_pJ, marker='.', label=rpr_alloc)
+        
+        ######################################
 
 plt.legend()
 plt.ylim(bottom=0)
