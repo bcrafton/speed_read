correct sign for bias

src/static_rpr.py
@@ -181,8 +181,8 @@ def static_rpr(low, high, params, profile, nrow, q):
                 # mu, std = prob_err(p[wb], params['sigma'], params['adc'], rpr, np.ceil(nrow / rpr))
                 mu, std = expected_error(params=params, profile=profile, rpr=rpr, nrow=np.ceil(nrow / rpr), bias=bias_lut[rpr])
 
-                e = (scale / q) * 64 * std
-                e_mu = (scale / q) * 64 * mu
+                e = (scale / q) * 10 * std
+                e_mu = (scale / q) * 10 * mu
 
                 if rpr == low:
                     rpr_lut[xb][wb] = rpr

src/tb.py
@@ -55,17 +55,17 @@ arch_params1 = {
 'skip': [1],
 'alloc': ['block'],
 'narray': [2 ** 13],
-'sigma': [0.01, 0.10, 0.2, 0.3],
+'sigma': [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15],
 'cards': [1],
 'profile': [1],
-'rpr_alloc': ['dynamic', 'centroids']
+'rpr_alloc': ['dynamic', 'static']
 }
 
 arch_params2 = {
 'skip': [1],
 'alloc': ['block'],
 'narray': [2 ** 13],
-'sigma': [0.01, 0.10, 0.2, 0.3],
+'sigma': [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15],
 'cards': [0],
 'profile': [1],
 'rpr_alloc': ['dynamic']
@@ -77,10 +77,10 @@ arch_params = {
 'skip': [1],
 'alloc': ['block'],
 'narray': [2 ** 13],
-'sigma': [0.10],
+'sigma': [0.05],
 'cards': [1],
 'profile': [1],
-'rpr_alloc': ['static']
+'rpr_alloc': ['dynamic']
 }
 
 ############
@@ -164,7 +164,7 @@ results = []
 for r in thread_results:
     results.extend(r)
 
-# np.save('results', results)
+np.save('results', results)
 print ('time taken:', time.time() - start)
 
 ####