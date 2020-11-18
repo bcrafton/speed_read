87453985008c530b5d5f18a79e3e15d5c8ee1704
Brian A Crafton <bcrafton3@KL2125-01.ece.gatech.edu>
2020-11-18 13:24:40 -0500

    vgg11

--- src/tb.py
+++ src/tb.py
@@ -79,14 +79,14 @@ def run_command(x, y, model, params, return_list):
         
 ####
 
-model, x, y = load_resnet(num_example=1, array_params=array_params)
-# model, x, y = load_cifar(num_example=2, array_params=array_params)
+# model, x, y = load_resnet(num_example=1, array_params=array_params)
+model, x, y = load_cifar(num_example=1, array_params=array_params)
 
 ####
 
 start = time.time()
 
-load_profile_adc = True
+load_profile_adc = False
 
 if not load_profile_adc:
     profile = model.profile_adc(x=x)
