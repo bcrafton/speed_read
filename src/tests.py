253c6acdc68ef003722ce2b30453712df624b821
Brian Crafton <bcrafton3@gatech.edu>
2021-09-09 12:01:40 -0400

    clear out tests

--- src/tests.py
+++ src/tests.py
@@ -70,181 +70,53 @@ def CC():
     return array_params, arch_params
     
 #######################################################
-    
-def BB():
+'''
+def CC():
 
     array_params = {
     'bpa': 8,
     'bpw': 8,
-    'adc': 8,
-    'adc_mux': 8,
-    'wl': 128,
-    'bl': 128,
-    'offset': 128,
-    'max_rpr': 64,
-    }
-    
-    arch_params = {
-    'skip': [0, 1],
-    'alloc': ['layer', 'block'],
-    'narray': [5472, 2 ** 13, 1.5 * 2 ** 13, 2 ** 14, 1.5 * 2 ** 14],
-    'sigma': [0.01],
-    'cards': [0],
-    'profile': [0, 1],
-    'rpr_alloc': ['dynamic'],
-    'thresh': [1.00]
-    }
-
-    arch_params = perms(arch_params)
-    
-    return array_params, arch_params
-    
-#######################################################
-    
-def Thresh():
-
-    array_params = {
-    'bpa': 8,
-    'bpw': 8,
-    'adc': 8,
-    'adc_mux': 8,
-    'wl': 128,
-    'bl': 128,
-    'offset': 128,
-    'max_rpr': 64,
-    }
-    
-    arch_params1 = {
-    'skip': [1],
-    'alloc': ['block'],
-    'narray': [2 ** 13],
-    'cards': [1],
-    'profile': [1],
-    'rpr_alloc': ['static'],
-    'sigma': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20],
-    'thresh': [0.5, 2.00, 5.00]
-    }
-
-    arch_params2 = {
-    'skip': [1],
-    'alloc': ['block'],
-    'narray': [2 ** 13],
-    'sigma': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20],
-    'cards': [0],
-    'profile': [1],
-    'rpr_alloc': ['dynamic'],
-    'thresh': [1.00]
-    }
-
-    arch_params1 = perms(arch_params1)
-    arch_params2 = perms(arch_params2)
-    arch_params = arch_params1 + arch_params2
-    return array_params, arch_params
-    
-#######################################################
-    
-def CE():
-
-    array_params = {
-    'bpa': 8,
-    'bpw': 8,
-    'adc': 8,
-    'adc_mux': 8,
-    'wl': 128,
-    'bl': 128,
-    'offset': 128,
-    'max_rpr': 64,
-    }
-    
-    arch_params1 = {
-    'skip': [1],
-    'alloc': ['block'],
-    'narray': [2 ** 12, 1.5 * 2 ** 12, 2 ** 13, 1.5 * 2 ** 13],
-    'sigma': [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15],
-    'cards': [1],
-    'profile': [1],
-    'rpr_alloc': ['static'],
-    'thresh': [1.00]
-    }
-
-    arch_params2 = {
-    'skip': [1],
-    'alloc': ['layer'],
-    'narray': [2 ** 12, 1.5 * 2 ** 12, 2 ** 13, 1.5 * 2 ** 13],
-    'sigma': [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15],
-    'cards': [1],
-    'profile': [0],
-    'rpr_alloc': ['static'],
-    'thresh': [1.00]
-    }
-
-    arch_params1 = perms(arch_params1)
-    arch_params2 = perms(arch_params2)
-    arch_params = arch_params1 + arch_params2
-    return array_params, arch_params
-
-#######################################################
-
-def Simple():
-
-    array_params = {
-    'bpa': 8,
-    'bpw': 8,
-    'adc': 8,
+    'adc': 64,
     'adc_mux': 8,
     'wl': 256,
     'bl': 256,
     'offset': 128,
-    'max_rpr': 16,
-    'sar': True,
-    'max_step': 2
+    'max_rpr': 128,
     }
 
-    arch_params = {
-    'skip': [1],
-    'alloc': ['block'],
-    'narray': [128],
-    'lrs': [0.07], 
-    'hrs': [0.03],
-    'cards': [0, 1],
-    'profile': [0],
-    'rpr_alloc': ['static'],
-    'thresh': [0.1],
-    }
+    # 'hrs': [0.50 / 10., 0.48 / 18., 0.35 / 30.],
 
-    arch_params = perms(arch_params)
-    return array_params, arch_params
-
-#######################################################
-
-def dac2():
-
-    array_params = {
-    'bpa': 8,
-    'bpw': 8,
-    'adc': 8,
-    'adc_mux': 8,
-    'wl': 256,
-    'bl': 256,
-    'offset': 128,
-    'max_rpr': 64,
-    }
-
-    arch_params = {
+    arch_params1 = {
     'skip': [1],
     'alloc': ['block'],
     'narray': [2 ** 12],
+    'lrs': [0.02, 0.04, 0.06, 0.08, 0.10], 
+    'hrs': [0.03, 0.015],
     'cards': [1],
     'profile': [1],
     'rpr_alloc': ['static'],
-    'sigma': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20],
-    'thresh': [0.25, 1.00, 2.00]
+    'thresh': [0.10, 0.25],
     }
 
-    arch_params = perms(arch_params)
-    return array_params, arch_params
+    arch_params2 = {
+    'skip': [1],
+    'alloc': ['block'],
+    'narray': [2 ** 12],
+    'lrs': [0.02, 0.04, 0.06, 0.08], 
+    'hrs': [0.03, 0.015],
+    'cards': [0],
+    'profile': [1],
+    'rpr_alloc': ['static'],
+    'thresh': [0.25],
+    }
 
+    arch_params1 = perms(arch_params1)
+    arch_params2 = perms(arch_params2)
+    arch_params = arch_params1 + arch_params2
+    return array_params, arch_params
+'''
 #######################################################
+
 
 
 
